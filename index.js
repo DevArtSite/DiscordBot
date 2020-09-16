ec1909a845f06913e7d707926454d85a58a2266e
MNLaugh <contact@mnlaugh.com>
2020-09-17 00:31:54 +0200

    v2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports = require('./structures/ClientDiscordBot')
-module.exports.DiscordBot = require('./structures/ClientDiscordBot')
+module.exports.ClientDiscordBot = require('./structures/ClientDiscordBot')
 module.exports.Command = require('./structures/Command')
 module.exports.Module = require('./structures/Module')
